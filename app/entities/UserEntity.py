53787efa4733351f5b3a06715a38edaed2056863
yykzjh <1378453948@qq.com>
2021-01-05 18:22:26 +0800

    增加添加新用户的接口函数

--- app/entities/UserEntity.py
+++ app/entities/UserEntity.py
@@ -65,6 +65,32 @@ def update(id, password = None, role_id = None):
         return True
 
 
+'''
+description: 添加新用户
+author: yykzjh
+Date: 2021-01-05 18:19:59
+param {账号:str} id
+param {密码:str} password
+param {姓名:str} name
+param {性别:str} gender
+param {生日:date} birthday
+param {电话:str} mobile
+param {负责区域:str} area
+param {工资:float} salary
+param {角色id:int} role_id
+return {True/False}
+'''
+def insert(id, password, name=None, gender=None, birthday=None, mobile=None, area=None, salary=None, role_id=None)
+    user = User.query.filter_by(id=id).first()
+    if user == None:
+        newUser = User(id=id, password=password, name=name, gender=gender, birthday=birthday, mobile=mobile, area=area, salary=salary, role_id=role_id)
+        db.session.add(newUser)
+        db.session.commit()
+        return True
+    else:
+        return False
+    
+    
 
 
 # if __name__ == "__main__":
