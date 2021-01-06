b03ae5fe0d3560a55d8414c4aaa7c2de1c9efde9
yykzjh <1378453948@qq.com>
2021-01-06 16:18:51 +0800

    完成返回所有商品类别的结构树

--- app/entities/GoodEntity.py
+++ app/entities/GoodEntity.py
@@ -4,7 +4,7 @@ Date: 2021-01-04 09:03:32
 Description: 商品实体的方法接口
 '''
 from sqlalchemy import (or_, func, not_, and_ ) 
-from app.models import db, Category, to_json
+from app.models import db, Category, to_json, Good
 
 
 '''
@@ -36,3 +36,31 @@ def searchGoodsId(cat_id):
     print(allGoodsId)
     return allGoodsId
 
+
+'''
+description: 返回指定商品类别下的结构树数据
+author: yykzjh
+Date: 2021-01-06 16:05:29
+param {当前商品的id:int} id
+return {[dict]} dict:{id, name, parent, level}
+'''
+def categoryDetails(id):
+    # 查询当前类别的子类别对象列表,并转换成子类型字典列表
+    children = to_json(Category.query.filter_by(parent=id).all())
+    # 遍历并进行递归
+    for child in children:
+        if child['level'] < 4:
+            child['children'] = categoryDetails(child['id'])
+    return children
+
+
+'''
+description: 返回指定商品的信息
+author: yykzjh
+Date: 2021-01-06 16:15:32
+param {商品id:int} good_id
+return {Good}
+'''
+def goodDetails(good_id):
+    good = Good.query.get(good_id).to_dict()
+    return good
