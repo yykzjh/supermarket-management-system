7047b7cafc5726f22c7b26eeabe0306dc8ae7c4f
yykzjh <1378453948@qq.com>
2021-01-06 10:42:25 +0800

    完成搜索分类下所有商品功能

--- app/entities/PurchaseOrderEntity.py
+++ app/entities/PurchaseOrderEntity.py
@@ -4,7 +4,9 @@ Date: 2021-01-04 09:05:21
 Description: 进货记录的方法接口
 '''
 
-from sqlalchemy import (or_, func)
-from app.models import db
+from sqlalchemy import (or_, func, and_, in_)
+from app.models import db, Purchase, to_json
 
 
+
+    
