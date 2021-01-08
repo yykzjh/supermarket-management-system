a25dbef1461befd93c6aef66f9d1c796c31a0c5c
yykzjh <1378453948@qq.com>
2021-01-08 20:44:53 +0800

    更正返回指定分类下各子分类的利润

--- app/controller_api/Statistic.py
+++ app/controller_api/Statistic.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, request, jsonify
-from app.entities.GoodEntity import searchGoodsId
+from app.entities.GoodEntity import searchNextCatId, theCatFixProfit
 from app.entities.PurchaseOrderEntity import expenditureInPeriod
 from app.entities.HistoryOrderEntity import revenueInPeriod
-import datetime
-from dateutil.relativedelta import relativedelta
+
 
 
 app_statistic = Blueprint("app_statistic", __name__)
@@ -19,23 +18,23 @@ description: 计算指定分类下前一天、前一月、前一年的利润
 author: yykzjh
 Date: 2021-01-08 19:46:22
 param {类别id:int} catId
-return JSON {day_profit:float, month_profit:float, year_profit:float}
+return JSON {StatusCode:200/400, data:[dict], msg:"该分类下没有子分类！"} 
+            dict:{id, name, parent, level, day_profit, month_profit, year_profit}
 '''
-@app_statistic.route("/TheCatFixRevenue", methods=["GET"])
-def theCatFixRevenue():
+@app_statistic.route("/NextCatsProfits", methods=["GET"])
+def nextCatsProfits():
     catId = request.args.get('catId')
-    # 获得该分类下所有商品id
-    goods = searchGoodsId(catId)
-    # 定义时间段
-    now_time = datetime.datetime.now()
-    before_day = now_time - datetime.timedelta(days=1)
-    before_month = now_time - relativedelta(months=+1)
-    before_year = now_time - relativedelta(years=+1)
-    # 计算不同时间段的利润
-    day_profit = revenueInPeriod(before_day, now_time, goods) - expenditureInPeriod(before_day, now_time, goods)
-    month_profit = revenueInPeriod(before_month, now_time, goods) - expenditureInPeriod(before_month, now_time, goods)
-    year_profit = revenueInPeriod(before_year, now_time, goods) - expenditureInPeriod(before_year, now_time, goods)
-    return jsonify(day_profit=day_profit, month_profit=month_profit, year_profit=year_profit)
+    # 初始化返回的数据
+    data = []
+    # 获取下级分类id列表
+    nextCatsId = searchNextCatId(catId)
+    if len(nextCatsId) == 0:
+        return jsonify(StatusCode=400, msg="该分类下没有子分类！")
+    # 分别求出利润
+    for nextId in nextCatsId:
+        data.append(theCatFixProfit(nextId))
+    return jsonify(StatusCode=200, data=data)
+    
 
 
 
