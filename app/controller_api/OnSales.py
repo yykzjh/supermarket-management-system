78489dbed9b3f00eb24ef58c1b0bf0705a9dd727
yykzjh <1378453948@qq.com>
2021-01-09 17:07:58 +0800

    返回所有上架记录、指定分类下的上架数量

--- app/controller_api/OnSales.py
+++ app/controller_api/OnSales.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from app.entities.OnSaleEntity import details, amountOfGoods
 
 
 app_on_sales = Blueprint("app_on_sales", __name__)
@@ -7,3 +8,32 @@ app_on_sales = Blueprint("app_on_sales", __name__)
 @app_on_sales.route("/Index")
 def index():
     return "on sales index"
+
+
+'''
+description: 返回所有上架记录
+author: yykzjh
+Date: 2021-01-09 17:04:35
+param {无}
+return {sales:[dict]} dict:{good_id, good_name, supplier_id, amount, price_out}
+'''
+@app_on_sales.route("/AllSales", methods=["GET"])
+def allSales():
+    sales = details()
+    return jsonify(sales=sales)
+
+
+'''
+description: 返回指定分类下所有商品的上架数量，直接指定商品也可以用
+author: yykzjh
+Date: 2021-01-09 17:06:23
+param {caId}
+return {amount:flloat}
+'''
+@app_on_sales.route("/amountOfTheCat", methods=["GET"])
+def amountOFTheCat():
+    catId = request.args.get('catId')
+    goodsId = searchGoodsId(catId)
+
+    amount = amountOfGoods(goodsId)
+    return jsonify(amount=amount)
--- app/entities/OnSaleEntity.py
+++ app/entities/OnSaleEntity.py
@@ -5,4 +5,19 @@ Description: 在售商品实体的方法接口
 '''
 
 from sqlalchemy import (or_, func)
-from app.models import db
+from app.models import db, Sale, to_json, Good
+
+
+def details():
+    sales = to_json(Sale.query.all())
+    for sale in sales:
+        sale['good_name'] = Good.query.get(sale['good_id']).name
+    return sales
+
+
+def amountOfGoods(goodsId):
+    sales = Sale.query.filter(Sale.good_id.in_(goodsId)).all()
+    sum = 0.0
+    for sale in sales:
+        sum += sale.amount
+    return sum
